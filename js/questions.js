84dc0922f9e0a2da0ec8051b09da625f9f8fa83c
Farhan Ali <96770412+Farhan0874@users.noreply.github.com>
2022-01-05 20:38:23 +0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the course code of OOP?",
+    answer: "CS-409",
+    options: [
+      "CS-407",
+      "CS-405",
+      "CS-409",
+      "CS-402"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the course code of OS?",
+    answer: "CS-407",
+    options: [
+      "CS-402",
+      "CS-407",
+      "CS-405",
+      "CS-409"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the course code of DS?",
+    answer: "CS-405",
+    options: [
+      "CS-409",
+      "402",
+      "CS-407",
+      "CS-405"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the class teacher of OOP?",
+    answer: "Sir Khalil-ur Rehman",
+    options: [
+      "Sir Khalil-ur Rehman",
+      "Sir Ayaz Rafi",
+      "Sir Waseem",
+      "Sir Abdullah"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is HOD of CS Department?",
+    answer: "Sir Ashraf",
+    options: [
+      "Sir Ashraf",
+      "Sir Khalil-ur Rehman",
+      "Sir Farooq Khalid",
+      "Sir Waseem"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
